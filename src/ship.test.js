616743eeb164e97a1639a8190dd41f37c747b8d5
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-06-06 13:36:06 -0700

    add hit function test

--- src/ship.test.js
+++ src/ship.test.js
@@ -3,3 +3,7 @@ import { createShip } from './ship';
 test('Ship factory creates an object that includes length, #/hits, and if sunk', () => {
     expect(createShip(3)).toStrictEqual({length: 3, hits: 0, sunk: false});
 })
+
+test('hit function increases number of hits by one', () => {
+    expect(createShip(3).hit()).toStrictEqual({length: 3, hits: 1, sunk: false});
+})
