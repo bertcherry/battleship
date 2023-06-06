801c578676cfcdea6e684cc9dca5f2c4d78a674c
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-06-06 13:17:26 -0700

    add ship factory test

---
+++ src/ship.test.js
@@ -0,0 +1,5 @@
+import { createShip } from './ship';
+
+test('Ship factory creates an object that includes length, #/hits, and if sunk', () => {
+    expect(createShip(3).ship).toBe({length: 3, hits: 0, sunk: false});
+})
