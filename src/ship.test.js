0bbda2b7342431794ab2234c7727a1f17926eb3e
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-06-06 17:47:46 -0700

    add sunk test

--- src/ship.test.js
+++ src/ship.test.js
@@ -2,8 +2,12 @@ import { createShip } from './ship';
 
 test('Ship factory creates an object that includes length, #/hits, and if sunk', () => {
     expect(createShip(3).ship).toStrictEqual({length: 3, hits: 0, sunk: false});
-})
+});
 
 test('hit function increases number of hits by one', () => {
     expect(createShip(3).hit()).toStrictEqual({length: 3, hits: 1, sunk: false});
-})
+});
+
+test('isSunk function changes sunk to false when hits match length', () => {
+    expect(createShip(1).hit()).toStrictEqual({length: 1, hits: 1, sunk: true});
+});
