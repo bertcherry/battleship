d48842b886eaf5e60566680bbceccaa5cd59bb04
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-06-07 11:29:34 -0700

    add receiveAttack test

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,11 +1,17 @@
 import { createGameboard } from './gameboard';
 import { createShip } from './ship';
 
+
+let args = ['a1', 'a2', 'a3'];
+const testBoard = createGameboard();
+testBoard.placeShip('destroyer', ...args);
+const destroyer = createShip('destroyer', 3).ship;
+
 test('Gameboard places ships at their coordinates by calling createShip', () => {
-    let args = ['a1', 'a2', 'a3'];
-    const testBoard = createGameboard();
-    testBoard.placeShip('destroyer', ...args);
-    const destroyer = createShip('destroyer', 3).ship;
-    //expect(testBoard.gameboard).toEqual({a1: 'destroyer', a2: 'destroyer', a3: 'destroyer'});
     expect(testBoard.gameboard.a2).toMatchObject(destroyer);
 });
+
+test('receiveAttack records a miss', () => {
+    testBoard.receiveAttack('b1');
+    expect(testBoard.shotList.includes('b1')).toBeTruthy();
+})
