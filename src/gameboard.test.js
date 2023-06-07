f2a938e6300b8965b068cbb000b4da5060484236
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-06-07 11:16:47 -0700

    fix new ship test

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,8 +1,11 @@
 import { createGameboard } from './gameboard';
+import { createShip } from './ship';
 
 test('Gameboard places ships at their coordinates by calling createShip', () => {
-    createGameboard().placeShip('destroyer', 'a1', 'a2', 'a3');
-    expect(createGameboard().gameboard.a1).toBe('destroyer');
-    expect(createGameboard().gameboard.a2).toBe('destroyer');
-    expect(createGameboard().gameboard.a3).toBe('destroyer');
+    let args = ['a1', 'a2', 'a3'];
+    const testBoard = createGameboard();
+    testBoard.placeShip('destroyer', ...args);
+    const destroyer = createShip('destroyer', 3).ship;
+    //expect(testBoard.gameboard).toEqual({a1: 'destroyer', a2: 'destroyer', a3: 'destroyer'});
+    expect(testBoard.gameboard.a2).toMatchObject(destroyer);
 });
