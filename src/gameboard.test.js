50fb01a935a9d4e8f469c82958a92245a5d27e17
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-06-08 12:54:09 -0700

    add all ships sunk test

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -12,4 +12,11 @@ test('receiveAttack records a miss', () => {
 test('receiveAttack records a hit', () => {
     testBoard.receiveAttack('a1');
     expect(testBoard.destroyer.ship.hits).toBe(1);
-})
+});
+
+test('reports if all ships are sunk', () => {
+    testBoard.receiveAttack('a1');
+    testBoard.receiveAttack('a2');
+    testBoard.receiveAttack('a3');
+    expect(testBoard.allSunk()).toBeTruthy();
+});
