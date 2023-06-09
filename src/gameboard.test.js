17b4cbcbdb31782b17e766d98c0fd1bb9d002326
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-06-09 13:54:18 -0700

    fix playGame called as object for test

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,37 +1,37 @@
-import { playGame } from './index';
+import { testGame } from './index';
 
 test('Gameboard places ships at their coordinates by calling createShip', () => {
-    expect(playGame().playerOneBoard.gameboard.a2).toEqual('destroyer');
+    expect(testGame.playerOneBoard.gameboard.a2).toEqual('destroyer');
 });
 
 test('receiveAttack records a miss', () => {
-    playGame().playerOneBoard.receiveAttack('b1');
-    expect(playGame().playerOneBoard.shotList.includes('b1')).toBeTruthy();
+    testGame.playerTwoBoard.receiveAttack('a1');
+    expect(testGame.playerTwoBoard.shotList.includes('a1')).toBeTruthy();
 });
 
 test('receiveAttack records a hit', () => {
-    playGame().playerOneBoard.receiveAttack('a1');
-    expect(playGame().playerOneBoard.destroyer.ship.hits).toBe(1);
+    testGame.playerOneBoard.receiveAttack('a1');
+    expect(testGame.playerOneBoard.destroyer.ship.hits).toBe(1);
 });
 
 //Skipped after refactoring test board to include all ships
 test.skip('reports if all ships are sunk', () => {
-    playGame().playerOneBoard.receiveAttack('a1');
-    playGame().playerOneBoard.receiveAttack('a2');
-    playGame().playerOneBoard.receiveAttack('a3');
-    expect(playGame().playerOneBoard.allSunk()).toBeTruthy();
+    testGame.playerOneBoard.receiveAttack('a1');
+    testGame.playerOneBoard.receiveAttack('a2');
+    testGame.playerOneBoard.receiveAttack('a3');
+    expect(testGame.playerOneBoard.allSunk()).toBeTruthy();
 });
 
 //Skipped while working on receive attack
 test('playerOne records hits on playerTwoBoard', () => {
-    playGame().playerOne.attack('b1');
-    expect(playGame().playerTwoBoard.shotList.includes('b1')).toBeTruthy();
+    testGame.playerOne.attack('b1');
+    expect(testGame.playerTwoBoard.shotList.includes('b1')).toBeTruthy();
 });
 
 //Skipped while working on receive attack
 test.skip('playerTwo (computer) does not attack the same place twice', () => {
     for (let i = 0; i < 40; i++) {
-        playGame().playerTwo.attack();     
+        testGame.playerTwo.attack();     
     };
-    expect((new Set(playGame().playerOneBoard.shotList)).size === playGame().playerOneBoard.shotList.length).toBeTruthy();
+    expect((new Set(testGame.playerOneBoard.shotList)).size === testGame.playerOneBoard.shotList.length).toBeTruthy();
 })
--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import { Player } from './player';
 import { createGameboard, playerOneArgs, playerTwoArgs } from './gameboard';
 
-function playGame() {
+const playGame = () => {
     //reverse initiation of players and boards once ship placement is in control of players
     const playerOneBoard = createGameboard(playerOneArgs);
     const playerTwoBoard = createGameboard(playerTwoArgs);
     const playerOne = Player(playerTwoBoard, false);
     const playerTwo = Player(playerOneBoard, true);
-    for (let i = 0; i < 49; i++) {
+    /* for (let i = 0; i < 49; i++) {
         playerOne.attack();
         playerTwo.attack();
         if (playerOneBoard.allSunk() || playerTwoBoard.allSunk()) {
             //end game
             break;
         }
-    }
+    } */
 
     return { playerOneBoard, playerTwoBoard, playerOne, playerTwo };
 }
 
-export { playGame };
+const testGame = playGame();
+
+export { testGame };
